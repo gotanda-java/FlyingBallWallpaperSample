f907b15dfc2784eb38bf24ac07ffe6c444d83281
kurata1016 <kurata1016@gmail.com>
2014-06-24 11:14:17 +0900

    add ball

--- src/jp/co/techfun/flyingballwallpaper/FlyingBallEffect.java
+++ src/jp/co/techfun/flyingballwallpaper/FlyingBallEffect.java
@@ -8,12 +8,14 @@ public class FlyingBallEffect {
 	// 飛び回る玉
 	private Ball ball;
 	private Ball ball2;
+	private Ball ball3;
 
 	// コンストラクタ
 	public FlyingBallEffect() {
 		// 玉オブジェクトを生成
 		ball = new Ball();
 		ball2 = new Ball();
+		ball3 = new Ball();
 	}
 
 	// drawメソッド(壁紙描画処理)
@@ -26,8 +28,11 @@ public class FlyingBallEffect {
 				(int) ball.ballY + ball.ballDiameter);
 		ball2.setBounds((int) ball2.ballX, (int) ball2.ballY, (int) ball2.ballX + ball2.ballDiameter,
 				(int) ball2.ballY + ball2.ballDiameter);
+		ball3.setBounds((int) ball3.ballX, (int) ball3.ballY, (int) ball3.ballX + ball3.ballDiameter,
+				(int) ball3.ballY + ball3.ballDiameter);
 		ball.draw(canvas);
 		ball2.draw(canvas);
+		ball3.draw(canvas);
 
 		canvas.restore();
 
@@ -43,6 +48,12 @@ public class FlyingBallEffect {
 				|| ball2.ballY + ball2.ballDiameter <= 0){
 			
 			ball2.resetPosition();
+		}
+		
+		if (ball3.width < ball3.ballX || ball3.height < ball3.ballY || ball3.ballX + ball3.ballDiameter <= 0
+				|| ball3.ballY + ball3.ballDiameter <= 0){
+			
+			ball3.resetPosition();
 		}
 	}
 }
